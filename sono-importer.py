c2aa526b418628dea97842db7c3efc0319c9e071
Ulrich Sommer <mail@ulrich-sommer.de>
2021-05-10 23:17:08 +0200

    added isynet import functionality

--- sono-importer.py
+++ sono-importer.py
@@ -1,36 +1,92 @@
 #!./venv/bin/python3
 from PIL import Image, ImageDraw, ImageFont
+from pathlib import Path
 import os
+import datetime
 
 #set config variables
+debug = True
+
 font = ImageFont.truetype('arial.ttf', 23)
-path_gdt = './in'
-path_image_in = './in/image'
-path_image_out = './out'
-path_ignore = './ignore'
-file_gdt = 'mcsrparchiv23.gdt'
+path_gdt = Path('./in')
+path_image_in = Path('./in/image')
+path_image_out = Path('./out')
+path_ignore = Path('./ignore')
+file_gdt = Path('mcsrparchiv23.gdt')
+filename_isynetImport = 'Archiv23'
+path_isynetImport = Path('./out')
 listOfJunkFiles = ["$RECYCLE.BIN", ".AppleDouble",'.com.apple.timemachine.donotpresent', '.com.apple.timemachine.supported', '.DocumentRevisions-V100', '.dropbox.cache', '.dropbox', '.DS_Store', '.fseventsd', '.LSOverride', '.Spotlight-V100', '.TemporaryItems', '.Trashes', 'Desktop.ini', 'ehthumbs.db', 'Thumbs.db']
 
 
 
-def imprintImage(inpath, filename):
-    #parse GDT file
+def createIsynetImportFile(imagefile, patid):
+    dateOfExam = datetime.date.today().strftime("%d.%m.%y")
+    archivenumber = 0
+    archiveExists = True
+    while archiveExists:
+        archivenumber = archivenumber + 1
+        try:
+           isynetimportfilehandler = open(os.path.join(path_isynetImport, filename_isynetImport + '.' + str(archivenumber).zfill(3)), 'x', encoding='windows-1252')
+           archiveExists = False
+        except (FileExistsError):
+            archiveExists = True
+    if debug:
+        print('Writing to archive file: ' + os.path.join(path_isynetImport, filename_isynetImport + '.' + str(archivenumber).zfill(3)))
     try:
-        gdtfile = open(os.path.join(path_gdt, file_gdt), "r", encoding="windows-1252")
-        for line in gdtfile:
-            line = line.strip()
-            if line.startswith('0153101'):
-                surname = line.removeprefix('0153101')
-            elif line.startswith('0143102'):
-                firstname = line.removeprefix('0143102')
-            elif line.startswith('0173103'):
-                dob = line.removeprefix('0173103')
-    except:
-        print ('error reading GDT file, using standard values...')
-        surname = 'Doe'
-        name = 'John'
-        dob = '01012000'
+        isynetimportfilehandler.write('01380006100\n') # unknown purpose
+        isynetimportfilehandler.write('014810000138\n') # unknown purpose
+        isynetimportfilehandler.write('0176200' + dateOfExam + '\n') #Date of exam DD.MM.YY
+        isynetimportfilehandler.write('0143600' + patid + '\n') # PatID
+        isynetimportfilehandler.write('0136228SONO\n') # must be "SONO" for importing reasons
+        isynetimportfilehandler.write('0676230' + imagefile) # filename of archived image file
+        isynetimportfilehandler.close()
+    except OSError as e:
+        print("Error writing to Archive file: %s : %s" % (archivenumber, e.strerror))
 
+def parseGDT():
+        #parse GDT file
+    try:
+        gdtfile = open(path_gdt / file_gdt, 'r', encoding='windows-1252')
+        for line in gdtfile:            
+            line = line.strip()
+            if line.find('3101') == 3:
+                surname = line[7:]
+            elif line.find('3102') == 3:
+                firstname = line[7:]
+            elif line.find('3103') == 3:
+                dob = line[7:]
+            elif line.find('3000') == 3:
+                patid = line[7:]
+
+    except OSError as e:
+        print (e + 'error reading GDT file, using standard values...')
+        surname = 'Doe'
+        firstname = 'John'
+        dob = '01012000'
+        patid = '000000'
+
+    return([surname, firstname, dob, patid])
+
+
+def imprintImage(inpath, filename):
+    #read GDT
+    GDTreturn = parseGDT()
+    surname = GDTreturn[0]
+    firstname = GDTreturn[1] 
+    dob = GDTreturn[2]
+    patid = GDTreturn[3]
+    outpath = os.path.join(path_image_out, datetime.date.today().strftime("%Y-%m-%d"))
+
+    try:
+        os.makedirs(outpath)
+    except OSError:
+        print ("Creation of the directory %s failed" % outpath)
+    else:
+        print ("Successfully created the directory %s" % outpath)
+
+    if debug:
+        print('Read from GDT file: Surname: ' + surname + ', firstname: ' + firstname + ', DOB: ' + dob + ', PatID: ' + patid)
+        
     # format strings according to Samsung naming convertion
     sono_name = (surname + ', ' + firstname)
     sono_dob = dob[:2] + '-' + dob[2:]
@@ -44,22 +100,25 @@ def imprintImage(inpath, filename):
     overlayDob.text((666, 52), sono_dob, fill="#adadab", anchor="lb", font=font)
 
     #combine images
-    try:
-        sonoImage = Image.open(os.path.join(inpath, filename))
-        sonoImage.paste(textoverlayImage, (0, 0), textoverlayImage)
-        sonoImage.save(os.path.join(path_image_out, filename),"TIFF")
-        print ('Imprinted ' + filename + ': ' + sono_name + ' ' + sono_dob)
-        return(True)
-    except:
-        print('Error imprinting image file...')
-        return(False)
+ #try:   
+    sonoImage = Image.open(os.path.join(inpath, filename))
+    sonoImage.paste(textoverlayImage, (0, 0), textoverlayImage)
+    sonoImage.save(os.path.join(outpath, patid + '-' + filename),"TIFF")
+    print ('Imprinted ' + os.path.join(outpath, patid + '-' + filename) + ': ' + sono_name + ' ' + sono_dob)
+    
+#except:
+    #print('Error imprinting image file...')
+    #return(False)
+    createIsynetImportFile(os.path.join(outpath, filename), patid)
+    return(True)
 
 for subdir, dirs, files in os.walk(path_image_in):
     for imagefile in files:
         if (imagefile.endswith('.Tiff') or imagefile.endswith('.tiff')):
-            if imprintImage(subdir, imagefile):
+            if imprintImage(str(subdir), imagefile):
                 try:
-                    os.remove(os.path.join(subdir, imagefile))
+                    #os.remove(os.path.join(subdir, imagefile))
+                    pass
                 except OSError as e:
                     print("Error deleting file: %s : %s" % (imagefile, e.strerror))
 
@@ -76,12 +135,6 @@ for subdir, dirs, files in os.walk(path_image_in):
         else:
             print ('Found antoher strange file, moving to IGNORE dir: ' + subdir + imagefile)
             try:
-                os.rename(os.path.join(subdir, imagefile), os.path.join(path_ignore, imagefile))
+                os.rename(os.path.join(subdir, imagefile), path_ignore / imagefile)
             except OSError as e:
                 print("Error moving file: %s : %s" % (imagefile, e.strerror))
-                
-            
-                
-            
-        
-        
