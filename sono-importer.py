62efdd4474dff05d0d8ff63feaf6c10c9a5a6df2
Ulrich Sommer <mail@ulrich-sommer.de>
2025-06-27 22:37:21 +0200

    Use ConfigParser getboolean for debug

--- sono-importer.py
+++ sono-importer.py
@@ -18,7 +18,7 @@ config_devices = configparser.ConfigParser()
 config_devices.read(base_dir / 'devices.conf')
 
 #set config variables
-debug = bool(config['Main']['debug'])
+debug = config.getboolean('Main', 'debug')
 device = config['Main']['device']
 
 font = ImageFont.truetype(base_dir / config_devices[device]['font'], int(config_devices[device]['font_size']))
